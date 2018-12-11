64152de36299ec6eb00715060ec0ff12c12bdbea
fwl <fwl@humio.com>
2018-12-11 10:48:25 +0100

    Changed log path.

--- Applications/ConsoleExample/ConsoleExample/Program.cs
+++ Applications/ConsoleExample/ConsoleExample/Program.cs
@@ -12,9 +12,8 @@ namespace ConsoleExample
     {
         static int Main(string[] args)
         {
-          //  var instanceId = Guid.NewGuid().ToString("N");
-            var instanceId = "";
-            var log = Path.Combine("ApplicationLogs", $"ConsoleExample-{instanceId}.txt"); // TODO: get from config.. env vars? 
+            var instanceId = Guid.NewGuid().ToString("N");
+            var log = Path.Combine("log", $"ConsoleExample-{instanceId}.txt"); // TODO: get from config.. env vars? 
 
             Log.Logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
@@ -22,24 +21,23 @@ namespace ConsoleExample
                 .WriteTo.File(new RenderedCompactJsonFormatter(),log, rollingInterval: RollingInterval.Day)
                 .CreateLogger();
 
-
             try
             {
-                while(true)
+                for (int i = 0; i < int.MaxValue; i++)
                 {
-                    Log.Information("Printing out {@Number}", x);
+                    Log.Information("Printing out {@Number}", i);
                 }
             }
             catch (Exception ex)
             {
                 Log.Fatal(ex, "Terminated unexpectedly");
-                return 1;
-            }
+                }
             finally
             {
                 Log.CloseAndFlush();
             }
 
+            return 0;
         }
     }
 }
