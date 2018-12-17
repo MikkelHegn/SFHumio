b9c38eeffbca2e097783aeb8d3b84837d1b0584b
Mikkel Mørk Hegnhøj <mikhegn@microsoft.com>
2018-12-17 11:36:07 +0100

    Updating Console App

--- Applications/ConsoleExample/ConsoleExample/Program.cs
+++ Applications/ConsoleExample/ConsoleExample/Program.cs
@@ -1,3 +1,85 @@
+<<<<<<< HEAD
+﻿using Newtonsoft.Json;
+using Serilog;
+using Serilog.Formatting.Compact;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleExample
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var instanceId = Guid.NewGuid().ToString("N");
+            var log = Path.Combine("log", $"ConsoleExample-{instanceId}.log"); // TODO: get from config.. env vars? 
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id.ToString())
+                .Enrich.WithProperty("ServiceFabricInfo", GetServiceFabricInfo())
+                .WriteTo.Console()
+                .WriteTo.File(new RenderedCompactJsonFormatter(), log, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                Random random = new Random();
+
+                while (true)
+                {
+                    int i = random.Next(100, 120);
+                    Log.Information("Number of user sessions {@UserSessions}", i);
+
+                    if (i == 120)
+                    {
+                        int ei = random.Next(0, 1000);
+                        if (ei == 42)
+                        {
+                            int ooops = ei / 0;
+                        }
+                    }
+
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            return 0;
+        }
+
+        private static string GetServiceFabricInfo()
+        {
+            ServiceFabricInfo sfInfo = new ServiceFabricInfo
+            {
+                NodeName = Environment.GetEnvironmentVariable("Fabric_NodeName"),
+                ServiceName = Environment.GetEnvironmentVariable("Fabric_ServiceName"),
+                ApplicationName = Environment.GetEnvironmentVariable("Fabric_ApplicationName"),
+                ServicePackageActivationId = Environment.GetEnvironmentVariable("Fabric_ServicePackageActivationId")
+            };
+
+            return JsonConvert.SerializeObject(sfInfo); ;
+        }
+
+        private class ServiceFabricInfo
+        {
+            public string NodeName { get; set; }
+            public string ServiceName { get; set; }
+            public string ApplicationName { get; set; }
+            public string ServicePackageActivationId { get; set; }
+        }
+    }
+}
+=======
 ﻿using System;
 using System.IO;
 using System.Linq;
@@ -41,3 +123,4 @@ namespace ConsoleExample
         }
     }
 }
+>>>>>>> 97bc68cba8bc962f076aceefcc318d8a71aa1cb8
