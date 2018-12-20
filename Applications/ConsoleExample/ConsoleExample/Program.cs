4dfa34c63a8ed00d667e5b057ed5f6ac0a904d69
fwl <fwl@humio.com>
2018-12-20 11:45:47 +0100

    Fixed conflicts.

--- Applications/ConsoleExample/ConsoleExample/Program.cs
+++ Applications/ConsoleExample/ConsoleExample/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Newtonsoft.Json;
 using Serilog;
 using Serilog.Formatting.Compact;
@@ -79,48 +78,3 @@ namespace ConsoleExample
         }
     }
 }
-=======
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using Serilog;
-using Serilog.Formatting.Compact;
-using Serilog.Formatting.Json;
-
-namespace ConsoleExample
-{
-    class Program
-    {
-        static int Main(string[] args)
-        {
-            var instanceId = Guid.NewGuid().ToString("N");
-            var log = Path.Combine("log", $"ConsoleExample-{instanceId}.txt"); // TODO: get from config.. env vars? 
-
-            Log.Logger = new LoggerConfiguration()
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .WriteTo.File(new RenderedCompactJsonFormatter(),log, rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-
-            try
-            {
-                for (int i = 0; i < int.MaxValue; i++)
-                {
-                    Log.Information("Printing out {@Number}", i);
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Terminated unexpectedly");
-                }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-
-            return 0;
-        }
-    }
-}
->>>>>>> 97bc68cba8bc962f076aceefcc318d8a71aa1cb8
